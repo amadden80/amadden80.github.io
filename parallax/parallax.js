63075bbdd4b172c56d66277082eeb3c4767195d5
Andrew <amadden80@gmail.com>
2015-04-24 16:07:48 -0500

    Update parallax.js

--- parallax/parallax.js
+++ parallax/parallax.js
@@ -14,28 +14,28 @@ function updateBannerImagePostion(bannerImages){
 }
 
 
-$(window).on("scroll", function(){
-
-  var opacityLevel = $(window).scrollTop()/$(window).height();
-
-  $('.information-block').css("backgroundColor", "rgba(100,0,0,"+opacityLevel+")")
-  $(".hi").css("opacity", opacityLevel*2);
-
-  $(".left-head").css("right", .75*$(window).scrollTop());
-  $(".hi").css("left", $(window).scrollTop());
-
-  if($(window).scrollTop() % 10){
-    $(".left-head").attr('src', 'images/face.png')
-  } else{
-    $(".left-head").attr('src', 'images/ahh.png')
-  }
-
-  updateBannerImagePostion(bannerImages);
-
-});
-
-
 $(function(){
   var bannerImages = $('.banner-image');
   updateBannerImagePostion(bannerImages);
+  
+  $(window).on("scroll", function(){
+
+    var opacityLevel = $(window).scrollTop()/$(window).height();
+
+    $('.information-block').css("backgroundColor", "rgba(100,0,0,"+opacityLevel+")")
+    $(".hi").css("opacity", opacityLevel*2);
+
+    $(".left-head").css("right", .75*$(window).scrollTop());
+    $(".hi").css("left", $(window).scrollTop());
+
+    if($(window).scrollTop() % 10){
+      $(".left-head").attr('src', 'images/face.png')
+    } else{
+      $(".left-head").attr('src', 'images/ahh.png')
+    }
+
+    updateBannerImagePostion(bannerImages);
+
+});
+
 })
